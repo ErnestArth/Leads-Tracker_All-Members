c76515aadfb130555ae2f1ec19614f20ef61828e
Godson <godsonsese04@gmail.com>
2025-08-11 15:38:59 +0000

    angular cdj and material verrsion fix

--- src/app/admin/components/edit-team-dialog/edit-team-dialog.component.ts
+++ src/app/admin/components/edit-team-dialog/edit-team-dialog.component.ts
@@ -1,25 +1,47 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-
+import { UserService,getAllTeamLeads } from '../../../services/user.service';
+import { FormGroup,FormBuilder,Validators} from '@angular/forms';
 @Component({
   selector: 'app-edit-team-dialog',
   standalone: false,
   templateUrl: './edit-team-dialog.component.html',
-  styleUrl: './edit-team-dialog.component.css'
+  styleUrl: './edit-team-dialog.component.css',
 })
 export class EditTeamDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
-private dialogRef: MatDialogRef<EditTeamDialogComponent>) { 
-  // console.log(data.id)
-}
+  allTeamLeads: any = [];
+  editTeamForm!: FormGroup;
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<EditTeamDialogComponent>,
+    private userService: UserService,
+    private fb: FormBuilder
+  ) {
+    // console.log(data.id)
+  }
 
-ngOnInit(): void {
-  console.log(this.data.id)
-}
+  ngOnInit(): void {
+    this.editTeamForm = this.fb.group({
+      name: ['', Validators.required],
+      teamLeadUserId: ['', Validators.required],
+    })
 
 
 
-closeModal(){
-  this.dialogRef.close();
-}
+    console.log(this.data.id);
+    this.getAllTeamLeads();
+  }
+
+  getAllTeamLeads() {
+    this.userService.getAllTeamLeads().subscribe({
+      next: (data) => {
+        this.allTeamLeads = data;
+        console.log(data);
+      },
+    })
+  }
+
+  closeModal() {
+    this.dialogRef.close();
+  }
 }
