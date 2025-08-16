620651245b67f1e174d86ebe6f47b568004122e0
Godson <godsonsese04@gmail.com>
2025-08-16 09:55:54 +0000

    setback fixed

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -503,12 +503,12 @@ export class UserService {
     );
   }
 
-  getClientsUnderUser(userId: string, searchTerm: string, status: string,duration: string,page: number , limit: number ) :Observable<getAllClients> {
+  getClientsUnderUser(userId: string, name: string, status: string,duration: string,page: number , limit: number ) :Observable<getAllClients> {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
     return this.http.get<getAllClients>(
-      `${this.apiUrl}/leads-tracker/leads-tracker/api/v1/clients/all-clients/${userId}?page=0&limit=5`,
-      { headers, params:{searchTerm, status,duration,page,limit} }
+      `${this.apiUrl}/leads-tracker/api/v1/clients/all-clients/${userId}`,
+      { headers, params:{name, status,duration,page,limit} }
     );
   }
 
